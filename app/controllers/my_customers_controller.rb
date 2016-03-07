285b8a68becd88d6dde3c38a5fc1d85100e44479
sakko <saklism@gmail.com>
2016-03-07 20:08:18 +0700

    fixed my customer create

--- app/controllers/my_customers_controller.rb
+++ app/controllers/my_customers_controller.rb
@@ -22,8 +22,7 @@ class MyCustomersController < ApplicationController
 
   # GET /my_customers/1/edit
   def edit
-    @company = @my_customer.tradeable if @my_customer.tradeable.class == Company
-    @company ||= Company.new
+    @company = @my_customer.tradeable.class == Company ? @my_customer.tradeable : Company.new
     @financer = @my_customer.tradeable if @my_customer.tradeable.class == Financer
     @financer ||= Financer.new
     @individual = @my_customer.tradeable if @my_customer.tradeable.class == Individual
@@ -33,10 +32,32 @@ class MyCustomersController < ApplicationController
   # POST /my_customers
   # POST /my_customers.json
   def create
-    @my_customer = MyCustomer.new(my_customer_params)
+    saved = false
+    begin
+      ActiveRecord::Base.transaction do
+        @my_customer = MyCustomer.new(my_customer_params)
+        if params[:my_customer][:tradeable_type] == "Company"
+          @company = Company.new(company_params)
+          @company.save!
+          @my_customer = @company
+        elsif params[:my_customer][:tradeable_type] == "Financer"
+          @financer = Financer.new(financer_params)
+          @financer.save!
+          @my_customer = @financer
+        elsif params[:my_customer][:tradeable_type] == "Individual"
+          @individual = Individual.new(individual_params)
+          @individual.save!
+          @my_customer = @individual
+        end
+        @my_customer.save!
+        saved = true
+      end
+    rescue ActiveRecord::RecordInvalid
+    end
+
 
     respond_to do |format|
-      if @my_customer.save
+      if saved
         format.html { redirect_to @my_customer, notice: 'My customer was successfully created.' }
         format.json { render :show, status: :created, location: @my_customer }
       else
@@ -78,6 +99,18 @@ class MyCustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def my_customer_params
-      params.require(:my_customer).permit(:number, :tradeable_id, :tradeable_type)
+      params.require(:my_customer).permit(:number)
+    end
+
+    def financer_params
+      params.require(:financer).permit(:name)
+    end
+
+    def individual_params
+      params.require(:individual).permit(:first_name, :last_name)
+    end
+
+    def company_params
+      params.require(:company).permit(:tax_number, :name)
     end
 end
